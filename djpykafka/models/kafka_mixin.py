b02e910c39b7ff420c3cb1cc615586a0e5ce7b73
Manuel Stingl <manuel.stingl@lavita.de>
2022-07-27 20:51:25 +0100

    Add field to KafkaPublishMixin

--- djpykafka/models/kafka_mixin.py
+++ djpykafka/models/kafka_mixin.py
@@ -1,8 +1,16 @@
+from datetime import timedelta
 from django.db import models
 
 
 class KafkaPublishMixin(models.Model):
+    KAFKA_PUBLISH_TIMEDELTA = timedelta(seconds=10)
+
     last_kafka_publish_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def objects_kafka_publish_lag(cls):
+        return cls.objects.filter(models.Q(last_kafka_publish_at__isnull=True) | models.Q(last_kafka_publish_at__lt=models.F('updated_at') + cls.KAFKA_PUBLISH_TIMEDELTA))
 
     class Meta:
         abstract = True

f68684554e533a76c18725c7574b1b010f7cfde9
Manuel Stingl <manuel.stingl@lavita.de>
2022-08-30 20:11:01 +0200

    add option to listen on changes of submodels

--- djpykafka/events/publish.py
+++ djpykafka/events/publish.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional, Type, TypeVar
+from typing import Callable, List, Optional, Type, TypeVar
 from functools import partial, cached_property
 import warnings
 from kafka import KafkaProducer
@@ -8,6 +8,7 @@ from kafka.producer.future import FutureRecordMetadata
 from pydantic import BaseModel
 from django.dispatch import receiver, Signal
 from django.db.models.signals import post_save, post_delete
+from django.db.models.fields.related_descriptors import ReverseManyToOneDescriptor
 from django.db import models
 from sentry_sdk import Hub, start_span
 from django.db.utils import OperationalError
@@ -29,6 +30,16 @@ TDjangoModel = TypeVar('TDjangoModel', bound=models.Model)
 class EventPublisher:
     action = 'update'
 
+    orm_model: Type[TDjangoModel]
+    event_schema: Type[TBaseModel]
+    connection: KafkaProducer
+    topic: str
+    data_type: str
+    is_changed_included: bool
+    version: Version
+    type: str
+    submodels: List[str]
+
     def __init_subclass__(
         cls,
         orm_model: Type[TDjangoModel],
@@ -39,6 +50,7 @@ class EventPublisher:
         is_changed_included: bool = False,
         version: Version = (1, 0, 0),
         type: str = 'data',
+        submodels: Optional[List[str]] = None,
         **kwargs,
     ):
         super().__init_subclass__()
@@ -50,6 +62,7 @@ class EventPublisher:
         cls.data_type = data_type
         cls.version = version
         cls.type = type
+        cls.submodels = submodels or []
         cls.is_changed_included = is_changed_included
         cls.is_tenant_bound = hasattr(cls.orm_model, 'tenant_id')
         cls.logger = logging.getLogger(f'{cls.__module__}.{cls.__name__}')
@@ -222,15 +235,40 @@ class DataChangePublisher(with_typehint(EventPublisher)):
         return getattr(DataChangeEvent.DataOperation, self.action.upper())
 
     @classmethod
-    def register(cls):
-        cls._handle_post_save = partial(cls.handle, signal=post_save)
-        receiver(post_save, sender=cls.orm_model)(cls._handle_post_save)
+    def _register(cls, model: Type[models.Model] = None, parent_foreignkeys: List[ReverseManyToOneDescriptor] = []):
+        save_handler_name = f'_handle_post_save_{model.__qualname__}'
+        def handle(sender, instance: TDjangoModel, signal: Signal, **kwargs):
+            for foreignkey in parent_foreignkeys:
+                instance = getattr(instance, foreignkey.name)
+
+            return cls.handle(sender, instance, signal, **kwargs)
+
+        setattr(cls, save_handler_name, partial(handle, signal=post_save))
+        receiver(post_save, sender=model)(getattr(cls, save_handler_name))
 
         if cls._kwargs.get('is_post_delete_received', True):
-            cls._handle_post_delete = partial(cls.handle, signal=post_delete)
-            receiver(post_delete, sender=cls.orm_model)(cls._handle_post_delete)
+            delete_handler_name = f'_handle_post_delete_{model.__qualname__}'
+            setattr(cls, delete_handler_name, partial(handle, signal=post_delete))
+            receiver(post_delete, sender=model)(getattr(cls, delete_handler_name))
 
-        cls.logger.debug("Registered post_save + post_delete handlers for %s", cls.orm_model)
+        cls.logger.debug("Registered post_save + post_delete handlers for %s", model)
+
+    @classmethod
+    def _register_submodels(cls):
+        for reference in cls.submodels:
+            model = cls.orm_model
+            parent_foreignkeys = []
+            for ref in reference.split('.'):
+                descriptor: ReverseManyToOneDescriptor = getattr(model, ref)
+                parent_foreignkeys.insert(0, descriptor.rel.field)
+                model = descriptor.rel.related_model
+
+            cls._register(model, parent_foreignkeys)
+
+    @classmethod
+    def register(cls):
+        cls._register(cls.orm_model)
+        cls._register_submodels()
 
 
 class StatusChangePublisher(with_typehint(EventPublisher)):
